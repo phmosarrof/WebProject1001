10fcbac2ab7e6316002e6421e853888569c03c6f
Mosarrof Hossain <phmosharaf@gmail.com>
2018-05-24 01:19:29 -0400

    added new thing

---
+++ src/myFirstprogram/Hellofriend.java
@@ -0,0 +1,18 @@
+package myFirstprogram;
+
+public class Hellofriend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This project is for my study group and practiec");
+		
+		int a,b,c;
+		a = 10; b = 20;
+		c = a+b;
+		
+		System.out.println("Total =" +c);
+
+	}
+
+}
