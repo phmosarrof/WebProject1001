b65236aff45f0c5729d05372bb01e7913951647d
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-03 01:47:57 -0400

    I added casting and conversion

---
+++ src/myFirstprogram/DataTypeCast1.java
@@ -0,0 +1,33 @@
+package myFirstprogram;
+
+public class DataTypeCast1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*int x,z;
+		x = 10;
+		float y = 100.70f;
+		z = x % (int)y;
+		System.out.println("The value of Z is "+z);*/
+		
+		int a;
+		a = 20;
+		
+		//float d = 50.50;
+		
+		double b = 20.50123;
+
+		
+		long c = 1000000000;
+		
+		
+		System.out.println("\nThe value of a is "+a);
+		System.out.println("\nThe value of b is "+b);
+		System.out.println("\nAfter data type casting - int (a/b) = " +a/b);
+		System.out.println("\nAfter casting with data long int (a/c) is "+ a/c);
+		
+	
+	}
+
+}
--- src/myFirstprogram/Hellofriend.java
+++ src/myFirstprogram/Hellofriend.java
@@ -3,15 +3,18 @@ package myFirstprogram;
 public class Hellofriend {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("This project is for my study group and practic");
+		System.out.println("\nThis project is for my study group and practic.\n");
+		System.out.println("This program is initial program for the new user.\n");
+		System.out.println("If anyone concentrated his mind definitely he will do good.\n");
+		
+		// just an integer addition.
 		
 		int a,b,c;
 		a = 10; b = 20;
 		c = a+b;
 		
-		System.out.println("Total =" +c);
+		System.out.println("Total = " +c);
 		
 		
 
