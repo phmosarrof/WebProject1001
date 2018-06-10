cbd93229b419d8b81eca5a17d8596928aef0d36c
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-10 18:26:25 -0400

    For loop done

---
+++ src/myFirstprogram/Loopstatement01.java
@@ -0,0 +1,20 @@
+package myFirstprogram;
+
+public class Loopstatement01 {
+
+	public Loopstatement01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	for (int i=19;i>17;i--) {
+		
+		System.out.println("<> Islam is the complete code of life.");
+		System.out.println("<> We should follow the rules and regulation\n of Islam for the satisfaction of Allah.");
+		
+		}
+	}
+
+}
