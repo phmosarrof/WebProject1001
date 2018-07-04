c371bc00baed82d2a614d428c3b977477d90bedc
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-04 12:00:58 -0400

    New code added

--- src/myFirstprogram/Human.java
+++ src/myFirstprogram/Human.java
@@ -1,33 +1,29 @@
 package myFirstprogram;
 
 public class Human {
+	
 	String Name;	
-	char gender; // Member Variable declare.
+	char gender; 			// Member Variable declare.
 	int age;
 	
 	public String getName() {
 		return Name;
 	}
-	
 	public void setName(String Name) {
 		this.Name = Name;
 	}
 	public char getGender() { // Member function declare.
 		return gender;
 	}
-	
 	public void setGender(char gender) { // Method
 		this.gender = gender;
 	}
-
 	public int getAge() {
 		return age;
 	}
-	
 	public void setAge(int age) {
 		this.age = age;
-	}
-		
+	}	
 	public static void main(String[] args) {
 		
 		Human mahdi = new Human();
@@ -35,18 +31,20 @@ public class Human {
 		
 		((Human) mahdi).setName("Mahdi Islam");
 		((Human) munim).setName("Munium Islam");
-		((Human) mahdi).setAge(25);
-		((Human) munim).setAge(15);
+		
+		mahdi.setAge(25);
+		munim.setAge(15);
 		
 		((Human) munim).setGender('M');
 		((Human) mahdi).setGender('M');
 		
-		System.out.println("Mahdi's Age = " + mahdi.getAge());
-		System.out.println("Munim's Age = " + munim.getAge());
-		System.out.println("Munim's Gender = " + munim.getGender());
-		System.out.println("Mahdi's Gender = " + mahdi.getGender());
+		
 		System.out.println("Mahdi's Full Name = "+ mahdi.getName());
+		System.out.println("Mahdi's Gender = " + mahdi.getGender());
+		System.out.println("Mahdi's Age = " + mahdi.getAge());
 		System.out.println("Munim's Full Name = "+ munim.getName());
+		System.out.println("Munim's Gender = " + munim.getGender());
+		System.out.println("Munim's Age = " + munim.getAge());
 	}
 
 }
